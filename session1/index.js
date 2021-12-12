bda68b28ecb8b8cdfaa00914b7472a26c0ed78a2
Kayla Newlon <kayla.b.newlon@gmail.com>
2021-12-12 13:17:18 -0800

    fizzBuzz complete

--- session1/index.js
+++ session1/index.js
@@ -100,7 +100,7 @@ function maxCharacter(str) {
 
     //for in loop used to loop through object instead of array
     for (let char in charMap) {
-        if(charMap[char] > maxNum){
+        if (charMap[char] > maxNum) {
             maxNum = charMap[char];
             maxChar = char;
         }
@@ -118,11 +118,19 @@ function maxCharacter(str) {
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() { 
-for(){
-    
-}
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 ===0 && i % 5 === 0) {
+            console.log('FizzBuzz')
+        } else if(i % 3 === 0){
+            console.log("Fizz")
+        }else if(i % 5 === 0){
+            console.log("Buzz")
+        }else{
+            console.log(i)
+        }
 
+    }
 }
 
 
